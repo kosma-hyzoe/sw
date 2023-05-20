cfc7f4bc6457f9f44c3fccd3287ad19c7cc61976
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-05-20 16:30:17 +0200

    automated quick commit

--- src/main/java/training/bits/friends/Friends.java
+++ src/main/java/training/bits/friends/Friends.java
@@ -9,18 +9,18 @@ public class Friends {
 	public Friends() {
 		this.employees = new long[EMPLOYEE_COUNT];
 		for (int i = 0; i < EMPLOYEE_COUNT; i++)
-			this.employees[i] = (1L << i);
+			this.employees[i] = 0L;
 	}
 
-	void addFriend(int a, int b) {
+	public void addFriend(int a, int b) {
 		this.employees[a] |= (1L << b);
 		this.employees[b] |= (1L << a);
 	}
 
-	boolean haveCommonFriend(int a, int b) {
-		long aFriendsExceptB = this.employees[a] & ~(1L << a) & ~(1L << b);
-		long bFriendsExceptA = this.employees[b] & ~(1L << a) & ~(1L << b);
-		return ((aFriendsExceptB & bFriendsExceptA) > 0) ? true : false;
+	public boolean haveCommonFriend(int a, int b) {
+		long aFriendsExceptB = this.employees[a] & ~(1L << b);
+		long bFriendsExceptA = this.employees[b] & ~(1L << a);
+		return (aFriendsExceptB & bFriendsExceptA) != 0;
 	}
 
 	public static void main(String[] args) throws IOException {
