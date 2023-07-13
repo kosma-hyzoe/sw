8f255ff2f03a6e5a8f7fbc7725ea53ee34d71d8e
kosma-hyzoe <kosma.hyzoe@gmail.com>
2023-07-13 15:43:35 +0200

    automated quick commit

--- src/wip/bfs/BFS.java
+++ src/wip/bfs/BFS.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 
 public class BFS {
 	private static int minNoOfTransfers = Integer.MAX_VALUE;
-	private static ArrayList<char[]> visited = new ArrayList<>();
 
 	// Not part of task - may be useful when debugging.
 	void printTestCase(int n, int m, char[] start, char[] end, char[][] a, char[][] b) {
@@ -14,8 +13,8 @@ public class BFS {
 		System.out.println("QUESTION: " + String.valueOf(start) + " -> " + String.valueOf(end));
 	}
 
-	public void backtrackTransfers(char[] curr, int nt, int n, int m, char[] end, char[][] a, char[][] b) {
-
+	public void backtrackTransfers(ArrayList<char[]> visited, char[] curr, int nt,
+							 	   int n, int m, char[] end, char[][] a, char[][] b) {
 		if (Arrays.equals(curr, end)) {
 			if (nt < minNoOfTransfers)
 				minNoOfTransfers = nt;
@@ -32,7 +31,8 @@ public class BFS {
 
 	public int solve(int n, int m, char[] start, char[] end, char[][] a, char[][] b) {
 		//printTestCase(n, m, start, end, a, b);
-		backtrackTransfers(start, 0, n, m, end, a, b);
+		ArrayList<char[]> visited = new ArrayList<>();
+		backtrackTransfers(visited, start, 0, n, m, end, a, b);
 
 		if (minNoOfTransfers == Integer.MAX_VALUE)
 			return -1;
